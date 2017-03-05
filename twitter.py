36548dd98824ce81144caaf54b68ff12021021a2
ysdyt <freedomandfavor@gmail.com>
2017-03-05 22:34:50 +0900

    first commit

---
+++ twitter.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from twython import Twython
+from twython import TwythonStreamer
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+            username = data['user']['screen_name']
+            tweet = data['text']
+            #print("@%s: %s" % (username, tweet))
+            print data
+
+stream = MyStreamer(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+stream.statuses.filter(track='#劇場版SAO')
